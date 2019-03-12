52a430b4f438ca28cefd5a2caf3b9c9c001b7278
snake-eye77 <asarade164@gmail.com>
2019-03-12 16:31:45 +0530

    all done

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,4 @@
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
-
-// const initialState = {};
-
-// const middleware = [thunk];
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   applyMiddleware(...middleware)
-// );
-
-// export default store;
-
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -25,10 +9,26 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(...middleware)
 );
 
 export default store;
+
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
+
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+// export default store;
